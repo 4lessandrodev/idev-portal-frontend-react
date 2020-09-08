93b82df00f95e0a1417515327fee3106e52f28bb
Alessandro <alessandroadm@live.com>
2020-09-07 23:27:03 -0300

    feat(components): adding button on questionnaire

--- src/components/Aula/Questionario/Questionario/Questionario.jsx
+++ src/components/Aula/Questionario/Questionario/Questionario.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Questionario.css';
 import Ask from '../Ask/Ask';
+import Button from '../../../Button/Button';
 
 export default props => (
     <section className="questionario">
-        <Ask description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate similique ab quibusdam quam eaque repellat quos, assumenda doloremque." tryNumber={2}/>
+        <form>   
+            <Ask description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate similique ab quibusdam quam eaque repellat quos, assumenda doloremque." tryNumber={2} />
+            <Button value="Confirmar"/>
+        </form>
     </section>
 )
